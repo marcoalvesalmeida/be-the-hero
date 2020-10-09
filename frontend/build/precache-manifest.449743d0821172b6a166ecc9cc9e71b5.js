dbe5d53c6c56802390cb977289cc76b0f594bad5
marcoalvesalmeida <marcoalvesneto@gmail.com>
2020-10-09 17:52:36 -0300

    Deploy Github Pages

---
+++ frontend/build/precache-manifest.449743d0821172b6a166ecc9cc9e71b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622c5c094c13c3b0f214b2ea81bbc34b",
+    "url": "/be-the-hero/site/index.html"
+  },
+  {
+    "revision": "6dd8339ea445afc5577f",
+    "url": "/be-the-hero/site/static/css/main.2050b099.chunk.css"
+  },
+  {
+    "revision": "06eb3ba261b03dea1299",
+    "url": "/be-the-hero/site/static/js/2.7f046380.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/be-the-hero/site/static/js/2.7f046380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd8339ea445afc5577f",
+    "url": "/be-the-hero/site/static/js/main.a4b7265e.chunk.js"
+  },
+  {
+    "revision": "711399a11d19db7a776f",
+    "url": "/be-the-hero/site/static/js/runtime-main.bfb750e8.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/be-the-hero/site/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/be-the-hero/site/static/media/logo.7eea718b.svg"
+  }
+]);
